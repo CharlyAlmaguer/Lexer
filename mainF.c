0053e20d3abc1900462be2b7be4a66f27f3cf1a3
CharlyAlmaguer <calmaguer.almaguer@gmail.com>
2020-06-04 14:10:19 -0500

    Programa1

---
+++ mainF.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+extern int yyparse();
+extern FILE *yyin;
+
+int main(int argc, char** argv){
+    FILE *f;
+    if (argc < 2){
+        printf("Faltan Argumentos...\n");
+        return -1;
+    }
+
+    f = fopen(argv[1], "r");
+    if (!f){
+        printf("El archivo %s no se puede abrir...", argv[1]);
+        return -1;
+    }
+
+    yyin = f;
+    yyparse();
+    fclose(yyin);
+    return 0;
+}
